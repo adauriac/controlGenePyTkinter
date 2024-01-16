9ca0631317b93efa330a7607078352f293573e8e
U-DESKTOP-5JA619C\ACXYS <jeanchristian@anglesdauriac.fr>
2024-01-16 19:48:18 +0100

    mesures OK

--- controlGenePyTkinter.py
+++ controlGenePyTkinter.py
@@ -1,5 +1,6 @@
 import controlGenePyCli
 from tkinter import *
+from tkinter import ttk
 
 myGene = controlGenePyCli.geneControler()
 ans = myGene.connect()
@@ -7,7 +8,6 @@ if not ans:
     msg = myGene.messageConnection
     print(msg)
     root = Tk()
-    root2 = Tk()
 
     label = Label(root,text=msg,height=20,width=100)
     label.pack()
@@ -15,38 +15,49 @@ if not ans:
     exit(1)
 
 root = Tk()
-canvasBoutonLed = Canvas(root,width=100*2,height=2*120*2,background='#F0F0F0')
-canvasConsignes = Canvas(root,width=100*2,height=2*120,background='#FEF0F0')
-canvasValues = Canvas(root,width=100*2,height=2*120,background='#F0FEF0')
-
+w1 = 250 # 200
+h1 = 520 #4*120
+w2 = w1
+h2 = h1/2 
+canvasBoutonLed = Canvas(root,width=w1,height=h1,background='#F0F0F0')
+canvasConsignes = Canvas(root,width=w2,height=h2,background='#FEF0F0')
+canvasValues = Canvas(root,width=w2,height=h1-h2,background='#F0FEF0')
 
 canvasBoutonLed.grid(row=0,column=0,rowspan=2)
 canvasConsignes.grid(row=0,column=1)
 canvasValues.grid(row=1,column=1)
 
-led = canvasBoutonLed.create_oval(50,50,100,100)
-canvasBoutonLed.itemconfig(led,fill='red')
-
 cpt = 0
 def watch():
-    global cpt,myGene,root
-    print("entering blink %d"%cpt)
+    global watchWaiting,cpt,myGene # car il sera modifie par getRegisters
+    myGene.getRegisters()
+    print("entering watch %d"%cpt)
     color = 'blue' if cpt%2 else 'red'
     cpt += 1
-    canvasBoutonLed.itemconfig(led,fill=color)
-    w = root.after(1000,watch)
+    canvasValues.itemconfig(ledVal,fill=color)
+    canvasValues.itemconfig(powerVal,text="%d"%myGene.vals[myGene.addToIndex[0x6B]])
+    canvasValues.itemconfig(flowVal,text="%d"%myGene.vals[myGene.addToIndex[0x68]])
+    canvasValues.itemconfig(currentVal,text="%d"%myGene.vals[myGene.addToIndex[0x7F]])
+    canvasValues.itemconfig(tensionVal,text="%d"%myGene.vals[myGene.addToIndex[0x72]])
+    root.after(1000,watch)
   
-canvasConsignes.create_text(40,10,text="Consignes")
+canvasConsignes.create_text(w2/2,h2/12,text="Consignes")
+nl = 12 # #lines
+R = 10 # rayon de la led
+canvasValues.create_text(w2/2,(h1-h2)/nl,anchor="center",text="Values")
+ledLab     = canvasValues.create_text(5,3*(h1-h2)/nl,anchor="w",text="Watchdog")
+powerLab   = canvasValues.create_text(5,5*(h1-h2)/nl,anchor="w",text="Puissance (W)")
+flowLab    = canvasValues.create_text(5,7*(h1-h2)/nl,anchor="w",text="Debit(1/mn)")
+currentLab = canvasValues.create_text(5,9*(h1-h2)/nl,anchor="w",text="Courant pont (mA)")
+tensionLab = canvasValues.create_text(5,11*(h1-h2)/nl,anchor="w",text="Tension PFC(V)")
+ledVal     = canvasValues.create_oval(3*w2/4-R/2,3*(h1-h2)/nl-R/2,3*w2/4+R/2,3*(h1-h2)/nl+R/2)
+powerVal   = canvasValues.create_text(3*w2/4+5,5*(h1-h2)/nl,anchor="center",text="111")
+flowVal    = canvasValues.create_text(3*w2/4+5,7*(h1-h2)/nl,anchor="center",text="222")
+currentVal = canvasValues.create_text(3*w2/4+5,9*(h1-h2)/nl,anchor="center",text="333")
+tensionVal = canvasValues.create_text(3*w2/4+5,11*(h1-h2)/nl,anchor="c",text="444")
 
-canvasValues.create_text(40,10,text="Values")
-canvasValues.create_text(40,10+1*30,text="Puissance (W)")
-#labValLeg=Label(canvasValues,text="Valeurs")
-#labValLeg.pack()
-#labValPow=Label(canvasValues,text="%d"%10)
-#labValPow.pack()
-canvasValues.create_text(40,10+2*30,text="Debit(1/mn)")
-canvasValues.create_text(40,10+3*30,text="Courant pont (mA)")
-canvasValues.create_text(40,10+4*30,text="Tension PFC(V)")
-
+# input("? ")
+# pour que le watchdog s'arrete quand on ferme le fenetre principale
+root.protocol("WM_DELETE_WINDOW",lambda : (root.after_cancel(watch),root.destroy()))
 watch()
 root.mainloop()
